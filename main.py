9d60b4b27db9415fd12502baff53d714f0a72cd6
Marquita Dunn <marquitaldunn@outlook.com>
2026-09-19 23:36:36 -0400

    Add main program flow and CSV ticket loading

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-from ast import If
-
+import csv
 
 print("IT Help Desk Ticket Analyzer")
 tickets = [
@@ -15,6 +14,18 @@ tickets = [
  "status": "pending"
 }
 ]
+def load_tickets():
+     tickets = []
+     with open("tickets.csv", "r") as file:
+        reader = csv.DictReader(file)
+        for row in reader:
+             ticket = {
+                 "id": int(row["id"]),
+                 "category": row["category"],
+                 "priority": row["priority"],
+                 "status": row["status"]
+             }
+             tickets.append(ticket)
 def display_tickets(tickets):
         for ticket in tickets:
             print("Ticket ID:", ticket["id"])
@@ -42,26 +53,28 @@ def search_ticket(tickets, search_id):
         if ticket["id"] == search_id:
             return ticket 
     return None
-try:
-    search_id = int(input("Enter Ticket ID: "))
-except ValueError:
-    print("Please enter a valid numeric Ticket ID.")
-    search_id = None
-display_tickets(tickets)
-open_count, closed_count, pending_count, high_priority_count = analyze_tickets(tickets)
-print("Total Open Tickets:", open_count)
-print("Total Closed Tickets:", closed_count)
-print("Total Pending Tickets:", pending_count)
-print("Total High Priority Tickets:", high_priority_count)
-if search_id is not None:
-        found_ticket = search_ticket(tickets, search_id)
-        if found_ticket is not None:
-            print("Ticket Found:")
-            print("Ticket ID:", found_ticket["id"])
-            print("Category:", found_ticket["category"])
-            print("Priority:", found_ticket["priority"])
-            print("Status:", found_ticket["status"])
-        else:
-            print("Ticket not found.")
 
+def main():
+    try:
+        search_id = int(input("Enter Ticket ID: "))
+    except ValueError:
+        print("Please enter a valid numeric Ticket ID.")
+        search_id = None
+    display_tickets(tickets)
+    open_count, closed_count, pending_count, high_priority_count = analyze_tickets(tickets)
+    print("Total Open Tickets:", open_count)
+    print("Total Closed Tickets:", closed_count)
+    print("Total Pending Tickets:", pending_count)
+    print("Total High Priority Tickets:", high_priority_count)
+    if search_id is not None:
+            found_ticket = search_ticket(tickets, search_id)
+            if found_ticket is not None:
+                print("Ticket Found:")
+                print("Ticket ID:", found_ticket["id"])
+                print("Category:", found_ticket["category"])
+                print("Priority:", found_ticket["priority"])
+                print("Status:", found_ticket["status"])
+            else:
+                print("Ticket not found.")
+main()
    
